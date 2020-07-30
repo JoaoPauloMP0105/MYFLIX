615e1d509ac9e2069c82e8f34c0a5e2a2b76a165
JoaoPauloMP0105 <joaopaulokbca@hotmail.com>
2020-07-30 13:04:46 -0300

    Alterando o logo no menu

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Menu.css';
 import Button from './components/ButtonLink';
 //import ButtonLink from '../../components/components/ButtonLink';
-import Logo from '../../assets/img/LogoMain.svg';
+import Logo from '../../assets/img/logo3.jpg';
 
 function Menu(){
     return (
