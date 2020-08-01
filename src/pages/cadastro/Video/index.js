70021050e0075ce7d61ee9c3e601928eba948002
JoaoPauloMP0105 <joaopaulokbca@hotmail.com>
2020-08-01 12:16:20 -0300

    instalando a lib do carrosel

--- src/pages/cadastro/Video/index.js
+++ src/pages/cadastro/Video/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
-import Menu from '../../../components/Menu';
-import Footer from '../../../components/Footer';
 import PageDefault from '../../../components/PageDefault';
 
 
